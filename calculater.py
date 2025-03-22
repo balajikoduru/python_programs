38dc79faea57158e4f81273b8d918b8dfc085b39
balaji <kodurubalaji1234@gmail.com>
2025-03-22 14:55:14 +0530

    updated calculater and other changes

--- calculater.py
+++ calculater.py
@@ -1,11 +1,1 @@
-[33mcommit cb8813cce62c357d22d6419f75f101e548f83c3b[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m)[m
-Author: balaji <kodurubalaji1234@gmail.com>
-Date:   Thu Mar 20 10:33:16 2025 +0530
-
-    this is python program
-
-[33mcommit 00f1a2ce4ef0129d934c3e20c068f35bebfc27bd[m[33m ([m[1;32mmain[m[33m)[m
-Author: balaji <kodurubalaji1234@gmail.com>
-Date:   Tue Mar 18 13:25:46 2025 +0530
-
-    this is my first commit
+q

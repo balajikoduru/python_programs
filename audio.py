c99d4adea0a042be6e44d1416cbf244e36788ecc
balaji <kodurubalaji1234@gmail.com>
2025-03-22 14:36:34 +0530

    audio updated

--- audio.py
+++ audio.py
@@ -3,13 +3,10 @@ from gtts import gTTS
 from playsound import playsound
 audio="speech1.mp3"
 language="en-US"
-text=("We’ve trained a model called ChatGPT which interacts in a conversational way."
-      " The dialogue format makes it possible for "
-      "ChatGPT to answer followup questions, admit "
-      "its mistakes, challenge incorrect premises, and reject "
-      "inappropriate requests.")
-sp=gtts.gTTS(text, lang=language, slow=False)
+text = ("hello")
+sp=gtts.gTTS(text, lang=language,tld="co.in", slow=False)
 sp.save(audio)
-playsound(audio)
+for i in range(2):
+      playsound(audio)
 print("playing the audio ")
 
